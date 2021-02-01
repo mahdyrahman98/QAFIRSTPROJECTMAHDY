80fabfc730b3630cc11dc63778008e2fe1e710bb
mahdyrahman98 <MRahman2@qa.com>
2021-02-01 17:31:29 +0000

    updating item controller commit -m

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -9,7 +9,7 @@ import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.Utils;
 
-public class ItemController implements CrudController<Item> {
+public abstract class ItemController implements CrudController<Item> {
 	
 
 	public static final Logger LOGGER = LogManager.getLogger();
@@ -19,21 +19,44 @@ public class ItemController implements CrudController<Item> {
 	private ItemDAO itemDAO;
 	private Utils utils;
 	
+	
+	@SuppressWarnings("rawtypes")
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;	
+		
+	}
+	
 
 	@Override
 	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Item> item = itemDAO.readAll();
+		for (Item items : item) {
+			LOGGER.info(items);
+			
+		}
+		return item;
 	}
 
-	@Override
-	public Item create() {
-		// TODO Auto-generated method stub
-		return null;
+	public Item create(Item item) {
+		LOGGER.info("Please enter the item name that you want");
+		String itemName = utils.getString();
+		LOGGER.info("Please enter the item price");
+		double price = utils.getDouble();
+		LOGGER.info( "Please enter the brand of your item");
+		String brand = utils.getString();
+		LOGGER.info("Item created");
+			// TODO Auto-generated method stub
+		return item;
 	}
 
 	@Override
 	public Item update() {
+		LOGGER.info("Please enter the id of the item would like to update");
+		Long id = utils.getLong();
+		LOGGER.info();
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -43,5 +66,8 @@ public class ItemController implements CrudController<Item> {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+
+
 
 }
