5a2e18c26eb116f1fa63aa6f5639ede303c04e3d
mahdyrahman98 <MRahman2@qa.com>
2021-02-01 16:35:34 +0000

    itemDAO class commited-mahdy

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,152 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+
+@SuppressWarnings("hiding")
+public class ItemDAO<Item> implements Dao<Item> {
+	
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	private static final String brand = null;
+	private static final String price = null;
+
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		int item_id = resultSet.getInt("item id");
+		String item_Name = resultSet.getString("item name");
+		String item_brand = resultSet.getString("brand");
+		double item_price =resultSet.getDouble("item price");
+		return new Item(item_brand, item_Name, brand, price);
+	}
+
+	/**
+	 * Reads all customers from the database
+	 * 
+	 * @return A list of customers
+	 */
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+			List<Item> Item = new ArrayList<>();
+			while (resultSet.next()) {
+				Item.add(modelFromResultSet(resultSet));
+			}
+			return Item;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Item readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM Items ORDER BY item_id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Creates a customer in the database
+	 * 
+	 * @param customer - takes in a customer object. id will be ignored
+	 */
+	@Override
+	public Item create(Item Item) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO Item (item_name, brand) VALUES (?, ?)");) {
+			statement.setString(1, Item.getItemName());
+			statement.setString(2, item.getItemBrand());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Item read(Long itemid) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM Items WHERE item_id = ?");) {
+			statement.setLong(1, itemid);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Updates a customer in the database
+	 * 
+	 * @param customer - takes in a customer object, the id field will be used to
+	 *                 update that customer in the database
+	 * @return
+	 */
+	@Override
+	public Item update(Item Item) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("UPDATE customers SET first_name = ?, surname = ? WHERE id = ?");) {
+			statement.setLong(1, Item.getItemid());
+			statement.setString(2, Item.getItemName());
+			statement.setString(3, Item.getBrand());
+			statement.setDouble(4, Item.getPrice());
+			statement.executeUpdate();
+			return read(.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes a customer in the database
+	 * 
+	 * @param id - id of the customer
+	 */
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM Item WHERE id = ?");) {
+			statement.setLong(1, id);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
+	
+	
+	
+	
+
+}
--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -3,17 +3,54 @@ package com.qa.ims.persistence.domain;
 public class Item {
 	
 	
-	private int item_id;
-	private String item_name;
+	private int itemid;
+	private String itemName;
+	private String brand;
+	private double price;
+	public Item(int itemid, String itemName, String brand, double price) {
+		super();
+		this.itemid = itemid;
+		this.itemName = itemName;
+		this.brand = brand;
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Item [itemid=" + itemid + ", itemName=" + itemName + ", brand=" + brand + ", price=" + price + "]";
+	}
+	public int getItemid() {
+		return itemid;
+	}
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((item_brand == null) ? 0 : item_brand.hashCode());
-		result = prime * result + item_id;
-		result = prime * result + ((item_name == null) ? 0 : item_name.hashCode());
+		result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + itemid;
 		long temp;
-		temp = Double.doubleToLongBits(item_price);
+		temp = Double.doubleToLongBits(price);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
@@ -26,61 +63,104 @@ public class Item {
 		if (getClass() != obj.getClass())
 			return false;
 		Item other = (Item) obj;
-		if (item_brand == null) {
-			if (other.item_brand != null)
+		if (brand == null) {
+			if (other.brand != null)
 				return false;
-		} else if (!item_brand.equals(other.item_brand))
+		} else if (!brand.equals(other.brand))
 			return false;
-		if (item_id != other.item_id)
-			return false;
-		if (item_name == null) {
-			if (other.item_name != null)
+		if (itemName == null) {
+			if (other.itemName != null)
 				return false;
-		} else if (!item_name.equals(other.item_name))
+		} else if (!itemName.equals(other.itemName))
 			return false;
-		if (Double.doubleToLongBits(item_price) != Double.doubleToLongBits(other.item_price))
+		if (itemid != other.itemid)
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
 			return false;
 		return true;
 	}
-	@Override
-	public String toString() {
-		return "Item [item_id=" + item_id + ", item_name=" + item_name + ", item_brand=" + item_brand + ", item_price="
-				+ item_price + "]";
-	}
-	public int getItem_id() {
-		return item_id;
-	}
-	public void setItem_id(int item_id) {
-		this.item_id = item_id;
-	}
-	public String getItem_name() {
-		return item_name;
-	}
-	public void setItem_name(String item_name) {
-		this.item_name = item_name;
-	}
-	public String getItem_brand() {
-		return item_brand;
-	}
-	public void setItem_brand(String item_brand) {
-		this.item_brand = item_brand;
-	}
-	public double getItem_price() {
-		return item_price;
-	}
-	public void setItem_price(double item_price) {
-		this.item_price = item_price;
-	}
-	public Item(int item_id, String item_name, String item_brand, double item_price) {
-		super();
-		this.item_id = item_id;
-		this.item_name = item_name;
-		this.item_brand = item_brand;
-		this.item_price = item_price;
-	}
-	private String item_brand;
-	private double item_price;
 	
 	
-
+	
 }
+	
+	
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((item_brand == null) ? 0 : item_brand.hashCode());
+//		result = prime * result + item_id;
+//		result = prime * result + ((item_name == null) ? 0 : item_name.hashCode());
+//		long temp;
+//		temp = Double.doubleToLongBits(item_price);
+//		result = prime * result + (int) (temp ^ (temp >>> 32));
+//		return result;
+//	}
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Item other = (Item) obj;
+//		if (item_brand == null) {
+//			if (other.item_brand != null)
+//				return false;
+//		} else if (!item_brand.equals(other.item_brand))
+//			return false;
+//		if (item_id != other.item_id)
+//			return false;
+//		if (item_name == null) {
+//			if (other.item_name != null)
+//				return false;
+//		} else if (!item_name.equals(other.item_name))
+//			return false;
+//		if (Double.doubleToLongBits(item_price) != Double.doubleToLongBits(other.item_price))
+//			return false;
+//		return true;
+//	}
+//	@Override
+//	public String toString() {
+//		return "Item [item_id=" + item_id + ", item_name=" + item_name + ", item_brand=" + item_brand + ", item_price="
+//				+ item_price + "]";
+//	}
+//	public int getItem_id() {
+//		return item_id;
+//	}
+//	public void setItem_id(int item_id) {
+//		this.item_id = item_id;
+//	}
+//	public String getItem_name() {
+//		return item_name;
+//	}
+//	public void setItem_name(String item_name) {
+//		this.item_name = item_name;
+//	}
+//	public String getItem_brand() {
+//		return item_brand;
+//	}
+//	public void setItem_brand(String item_brand) {
+//		this.item_brand = item_brand;
+//	}
+//	public double getItem_price() {
+//		return item_price;
+//	}
+//	public void setItem_price(double item_price) {
+//		this.item_price = item_price;
+//	}
+//	public Item(int item_id, String item_name, String item_brand, double item_price) {
+//		super();
+//		this.item_id = item_id;
+//		this.item_name = item_name;
+//		this.item_brand = item_brand;
+//		this.item_price = item_price;
+//	}
+//	private String item_brand;
+//	private double item_price;
+//	
+//	
+//
+//}
