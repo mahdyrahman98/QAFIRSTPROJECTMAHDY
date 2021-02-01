f6df3bfcb8e493ca1e3b136b601f44d6c2e88d10
mahdyrahman98 <MRahman2@qa.com>
2021-02-01 17:13:01 +0000

    Item controller started commit - m

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -1,5 +1,47 @@
 package com.qa.ims.controller;
 
-public class ItemController {
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+	
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	
+	@SuppressWarnings({ "unused", "rawtypes" })
+	private ItemDAO itemDAO;
+	private Utils utils;
+	
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
